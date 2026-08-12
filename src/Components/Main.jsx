881f2858485c4d4089ae88231edae0f2b8ec5283
Abduletif Abdella <abduletifabdella29@gmail.com>
2026-08-12 18:27:38 +0300

    feat: add responsive empty state and add button

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import search from '../assets/icons/search.svg'
 import bottomArrow from '../assets/icons/bottomArrow.png' 
-import moon from '../assets/icons/moon.svg' 
+import moon from '../assets/icons/moon.svg'
+import searchMan from '../assets/images/searchMan.png'
+import plus from '../assets/icons/plus.png'
 
 function Main() {
 return (
@@ -32,10 +34,19 @@ return (
         </div>
 
         <div>
-            <button className="bg-[#6C63FF] w-[px] lg:w-[px] h-9.5 rounded-lg ml-5 px-3">
+            <button className="bg-[#6C63FF] h-9.5 rounded-lg ml-5 px-3">
                 <img src={moon} className="w-5.5 h-5.5 object-contain" />
             </button>
         </div>
+    </div>
+
+    <div className='max-w-187.5 relative mx-auto'>
+        <img className='w-55.25 h-43.5 mt-7.5 mx-auto' src={searchMan} />
+        <h1 className='text-black font-normal text-[20px] text-center pt-5'>Empty...</h1>
+    
+        <button className="bg-[#6C63FF] w-12.5 h-12.5 rounded-full right-11 mt-25 absolute">
+            <img className="w-6 h-6 mx-auto" src={plus} />
+        </button>
     </div>
     </>
 )
