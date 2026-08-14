1117ac151d3b183f7eb8689b63105c1b6f54e3a6
Abduletif Abdella <abduletifabdella29@gmail.com>
2026-08-14 15:14:01 +0300

    feat: Add filter dropdown button

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -10,6 +10,9 @@ function Main() {
     const [darkMode, setDarkMode] = useState(() => {
     return localStorage.getItem('darkMode') === 'true';
     });
+
+    const [isOpen, setIsOpen] = useState(false)
+    const [filter, setFilter] = useState('ALL')
     
     useEffect(() => {
     localStorage.setItem('darkMode', darkMode);
@@ -27,7 +30,7 @@ return (
                 <input 
                     type="search" 
                     placeholder='Search note...' 
-                    className={`sm:w-120.75 lg:w-148.75 h-9.5 pl-3 pr-10 border-[1.4px] border-[#6C63FF] rounded-lg outline-none text-sm ${ darkMode
+                    className={`sm:w-95.75 lg:w-148.75 h-9.5 pl-3 pr-10 border-[1.4px] border-[#6C63FF] rounded-lg outline-none text-sm ${ darkMode
                         ? 'bg-gray-800 text-white placeholder:text-gray-300'
                         : 'bg-white text-black placeholder:text-gray-500'
                     }`} 
@@ -40,10 +43,50 @@ return (
             </div>
 
             <div className='relative ml-5'>
-                <button className="bg-[#6C63FF] lg:w-21.25 h-9.5 w-17 rounded-lg flex items-center gap-2 lg:gap-4 px-3 hover:bg-[#574DDB] transition-colors duration-200">
-                    <h1 className="text-white font-semibold text-[18px]">ALL</h1>
-                    <img src={bottomArrow} className="w-4.5 h-1 object-contain" />
+                <button
+                    onClick={() => setIsOpen(!isOpen)}
+                    className="bg-[#6C63FF] w-30 h-9.5 rounded-lg flex items-center justify-between px-1.5 hover:bg-[#574DDB] transition-colors duration-200"
+                >
+                    <h1 className="text-white font-semibold text-[18px]">
+                        {filter}
+                    </h1>
+
+                    <img
+                        src={bottomArrow}
+                        className={`w-4.5 h-1 object-contain transition-transform duration-200 ${isOpen ? 'rotate-180' : ''}`}
+                    />
                 </button>
+
+                {isOpen && (
+                    <div className="absolute top-10 left-0 w-30 bg-white border-2 border-[#6C63FF] rounded-lg overflow-hidden z-10">
+                        <button
+                            onClick={() => {
+                                setFilter('ALL')
+                                setIsOpen(false)
+                            }}
+                            className="w-full text-left px-2 py-1 text-[#6C63FF] text-[18px] hover:bg-[#c4c2f3]">
+                            All
+                        </button>
+
+                        <button
+                            onClick={() => {
+                                setFilter('Complete')
+                                setIsOpen(false)
+                            }}
+                            className="w-full text-left px-2 py-1 text-[#6C63FF] text-[18px] hover:bg-[#c4c2f3]">
+                            Complete
+                        </button>
+
+                        <button
+                            onClick={() => {
+                                setFilter('Incomplete')
+                                setIsOpen(false)
+                            }}
+                            className="w-full text-left px-2 py-1 text-[#6C63FF] text-[18px] hover:bg-[#c4c2f3]">
+                            Incomplete
+                        </button>
+                    </div>
+                )}
             </div>
 
             <div>
